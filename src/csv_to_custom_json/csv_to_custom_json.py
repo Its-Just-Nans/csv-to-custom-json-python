e85ce7cca2d0ef55e2a7a2d4a0ba1edb24e3c72b
n4n5 <its.just.n4n5@gmail.com>
2021-12-15 16:01:31 +0100

    [fix] go to pep8

--- src/csv_to_custom_json/csv_to_custom_json.py
+++ src/csv_to_custom_json/csv_to_custom_json.py
@@ -1,230 +1,231 @@
+""" csv_to_custom_json by n4n5 """
 from csv import reader as CSVreader
 from os.path import isfile
 from json import dumps as JSONstringify
 
+# pylint: disable=W0102
+# -> dangerous arguments
+# pylint: disable=C0103
+# -> Function name "parseFile"
 
-def parseFile(pathToFile="", schema=None, optionsUser={}):
-    global lineReader
-    def checkOptions(optionsUser, attr, defaultValue):
-        if attr in optionsUser:
-            return optionsUser[attr]
-        else:
-            return defaultValue
+def parseFile(path_to_file="", schema=None, options_user={}):
+    """Global function to parse a file"""
+    def check_options(options_user, attr, default_value):
+        """ Check options or put default value """
+        if attr in options_user:
+            return options_user[attr]
+        return default_value
     options = {
-        "arrayParse": checkOptions(optionsUser, "arrayParse", True),
-        "callBackForce": checkOptions(optionsUser, "callBackForce", False),
-        "debug": checkOptions(optionsUser, "debug", False),
-        "error": checkOptions(optionsUser, "error", False),
-        "lineCallBack": checkOptions(optionsUser, "lineCallBack", None),
-        "parse": checkOptions(optionsUser, "parse", True),
-        "separator": checkOptions(optionsUser, "separator", ","),
-        "privateSeparator": checkOptions(optionsUser, "privateSeparator", "..."),
-        "overrideFirstLine": checkOptions(optionsUser, "overrideFirstLine", False),
-        "avoidVoidLine": checkOptions(optionsUser, "avoidVoidLine", False)
+        "arrayParse": check_options(options_user, "arrayParse", True),
+        "callBackForce": check_options(options_user, "callBackForce", False),
+        "debug": check_options(options_user, "debug", False),
+        "error": check_options(options_user, "error", False),
+        "lineCallBack": check_options(options_user, "lineCallBack", None),
+        "parse": check_options(options_user, "parse", True),
+        "separator": check_options(options_user, "separator", ","),
+        "privateSeparator": check_options(options_user, "privateSeparator", "..."),
+        "overrideFirstLine": check_options(options_user, "overrideFirstLine", False),
+        "avoidVoidLine": check_options(options_user, "avoidVoidLine", False)
     }
     if options["debug"]:
-        if (isinstance(schema,list) or isinstance(schema, dict)) and schema != None:
+        if isinstance(schema, (list, dict)) and schema is not None:
             print("HAS SCHEMA")
         else:
             print("NO SCHEMA")
             print("OPTIONS", JSONstringify(options))
         if options["error"] == "no":
             print("Useless informations : just use try catch if you don't want error :)")
-    if isinstance(pathToFile, str):
-        if not isfile(pathToFile):
+    if isinstance(path_to_file, str):
+        if not isfile(path_to_file):
             if options["error"] == "no":
                 return []
-            else:
-                raise ValueError("Can't access to the file : '{}'".format(pathToFile))
+            raise ValueError("Can't access to the file : '{}'".format(path_to_file))
 
-    if isinstance(pathToFile, str):
-        csvFile = open(pathToFile)
-        lineReader = CSVreader(csvFile, delimiter=options["separator"])
-    elif isinstance(pathToFile, list):
-        lineReader = pathToFile
-    rows = []
-    firstLine = []
+    if isinstance(path_to_file, str):
+        csv_file = open(path_to_file)
+        line_reader = CSVreader(csv_file, delimiter=options["separator"])
+    elif isinstance(path_to_file, list):
+        line_reader = path_to_file
 
-    def createFieldsBinding(schemaObject, startPath=""):
-        global firstLine
+    def create_fields_binding(schema_object, first_line, start_path=""):
+        """ Create fields bindings """
         bindings = []
-        for index, value in enumerate(schemaObject):
-            if isinstance(schemaObject, list):
-                oneElement = index
-            elif isinstance(schemaObject, dict):
-                oneElement = value
-            if startPath == "":
-                path = '{}'.format(oneElement)
+        for index, value in enumerate(schema_object):
+            is_list = isinstance(schema_object, list)
+            is_dict = isinstance(schema_object, dict)
+            if is_list:
+                one_element = index
+            elif is_dict:
+                one_element = value
+            if start_path == "":
+                path = '{}'.format(one_element)
             else:
                 path = '{}{}{}'.format(
-                    startPath, options["privateSeparator"], oneElement)
-            if isinstance(schemaObject[oneElement], dict) or isinstance(schemaObject[oneElement], list):
-                if isinstance(schemaObject[oneElement], list):
+                    start_path, options["privateSeparator"], one_element)
+            if isinstance(schema_object[one_element], (dict, list)):
+                if isinstance(schema_object[one_element], list):
                     bindings.append({
-                        "name": oneElement,
+                        "name": one_element,
                         "path": path,
                         "type": "helper-array"
                     })
-                bindings = [
-                    *bindings, *createFieldsBinding(schemaObject[oneElement], path)]
+                bindings = bindings + create_fields_binding(schema_object[one_element], first_line, path)
             else:
-                if isinstance(schemaObject, list) and options["arrayParse"] and schemaObject[oneElement] in firstLine:
+                if is_list and options["arrayParse"] and schema_object[one_element] in first_line:
                     bindings.append({
-                        "name": schemaObject[oneElement],
+                        "name": schema_object[one_element],
                         "path": path,
                         "value": "string"
                     })
                 else:
-                    if oneElement in firstLine or callable(schemaObject[oneElement]):
+                    if one_element in first_line or callable(schema_object[one_element]):
                         bindings.append({
-                            "name": oneElement,
+                            "name": one_element,
                             "path": path,
-                            "value": schemaObject[oneElement]
+                            "value": schema_object[one_element]
                         })
                     else:
                         bindings.append({
-                            "name": oneElement,
+                            "name": one_element,
                             "path": path,
                             "type": "static",
-                            "value": schemaObject[oneElement]
+                            "value": schema_object[one_element]
                         })
         return bindings
 
-    def parseLine(line):
-        global rows
-        global firstLine
+    def parse_line(line, rows, first_line):
+        """" Parse one line """
         if isinstance(schema, list):
             obj = []
         else:
             obj = {}
-        allValues = line
-        for oneRow in rows:
-            onePathRow = oneRow["path"]
-            onePathName = oneRow["name"]
-            allPath = onePathRow.split(options["privateSeparator"])
-            currentValue = None
-            if ('type' not in oneRow) or ('type' in oneRow and oneRow["type"] == None):
-                if 'value' not in oneRow:
-                    schemaValue = None
+        all_values = line
+        for one_row in rows:
+            one_path_row = one_row["path"]
+            one_path_name = one_row["name"]
+            all_path = one_path_row.split(options["privateSeparator"])
+            current_value = None
+            if ('type' not in one_row) or ('type' in one_row and one_row["type"] is None):
+                if 'value' not in one_row:
+                    schema_value = None
                 else:
-                    schemaValue = oneRow["value"]
-                if oneRow["name"] in firstLine:
-                    index = firstLine.index(oneRow["name"])
+                    schema_value = one_row["value"]
+                if one_row["name"] in first_line:
+                    index = first_line.index(one_row["name"])
                 else:
                     index = -1
                 if index == -1:
-                    currentValue = schemaValue
+                    current_value = schema_value
                 else:
-                    if index < len(allValues):
-                        currentValue = allValues[index]
-                if options["parse"] == True and currentValue != None:
-                    if schemaValue == "int" and currentValue != '':
-                        currentValue = int(currentValue)
-                    elif schemaValue == "float":
-                        currentValue = float(currentValue)
-                    elif schemaValue == "string":
-                        currentValue = str(currentValue)
-                    elif callable(schemaValue):
-                        if callable(currentValue):
+                    if index < len(all_values):
+                        current_value = all_values[index]
+                if options["parse"] and current_value is not None:
+                    if schema_value == "int" and current_value != '':
+                        current_value = int(current_value)
+                    elif schema_value == "float":
+                        current_value = float(current_value)
+                    elif schema_value == "string":
+                        current_value = str(current_value)
+                    elif callable(schema_value):
+                        if callable(current_value):
                             # When the value is in an array
-                            currentValue = schemaValue(allValues)
+                            current_value = schema_value(all_values)
                         else:
-                            currentValue = schemaValue(currentValue)
-            elif ('type' in oneRow and oneRow["type"] == "helper-array"):
-                currentValue = []
-            elif ('type' in oneRow and oneRow["type"] == "static"):
-                currentValue = oneRow["value"]
-            goodPlace = None
-            if len(allPath) > 1:
-                goodPlace = obj
-                long = len(allPath)
+                            current_value = schema_value(current_value)
+            elif ('type' in one_row and one_row["type"] == "helper-array"):
+                current_value = []
+            elif ('type' in one_row and one_row["type"] == "static"):
+                current_value = one_row["value"]
+            good_place = None
+            if len(all_path) > 1:
+                good_place = obj
+                long = len(all_path)
                 for count in range(0, long):
-                    nextPath = allPath[count]
-                    if isinstance(goodPlace, list):
-                        nextPathInt = int(nextPath)
+                    next_path = all_path[count]
+                    if isinstance(good_place, list):
+                        next_path_int = int(next_path)
                     if count == (long - 1):
-                        if isinstance(goodPlace, dict):
-                            goodPlace[nextPath] = ""
+                        if isinstance(good_place, dict):
+                            good_place[next_path] = ""
                     else:
-                        if (isinstance(goodPlace, list) and nextPathInt not in goodPlace) or nextPath not in goodPlace:
-                            if isinstance(goodPlace, list):
-                                if len(goodPlace) < (nextPathInt+1):
+                        if (isinstance(good_place, list) and next_path_int not in good_place) or next_path not in good_place:
+                            if isinstance(good_place, list):
+                                if len(good_place) < (next_path_int+1):
                                     # len() returns 0 and the first index of the list is 0 !
-                                    goodPlace.insert(nextPathInt, {})
+                                    good_place.insert(next_path_int, {})
                             else:
-                                goodPlace[nextPath] = {}
-                        if isinstance(goodPlace, list):
-                            if nextPathInt < len(goodPlace):
-                                goodPlace = goodPlace[nextPathInt]
+                                good_place[next_path] = {}
+                        if isinstance(good_place, list):
+                            if next_path_int < len(good_place):
+                                good_place = good_place[next_path_int]
                         else:
-                            goodPlace = goodPlace[nextPath]
-                if isinstance(goodPlace, list):
-                    goodPlace.append(currentValue)
-                elif isinstance(goodPlace, dict):
-                    goodPlace[onePathName] = currentValue
+                            good_place = good_place[next_path]
+                if isinstance(good_place, list):
+                    good_place.append(current_value)
+                elif isinstance(good_place, dict):
+                    good_place[one_path_name] = current_value
                 else:
-                    goodPlace = currentValue
+                    good_place = current_value
             else:
                 if isinstance(obj, list):
-                    place = int(onePathRow)
-                    obj.insert(place, currentValue)
+                    place = int(one_path_row)
+                    obj.insert(place, current_value)
                 elif isinstance(obj, dict):
-                    obj[onePathRow] = currentValue
+                    obj[one_path_row] = current_value
         return obj
 
-    def parsefirstLine():
-        global firstLine
-        if isinstance(options["overrideFirstLine"], list):
-            firstLine = options["overrideFirstLine"]
-        if schema != None:
+    def parse_first_line(first_line):
+        """ Parse the first line """
+        if schema is not None:
             # None is default value for schema
-            cols = createFieldsBinding(schema)
+            cols = create_fields_binding(schema, first_line)
             if options["debug"]:
                 print("BINDINGS:", JSONstringify(cols, default=lambda o: '<not serializable>'))
         else:
             def dupli(element):
+                """" Duplicate the first line """
                 return {
                     "name": element,
                     "path": element
                 }
-            cols = [dupli(x) for x in firstLine]
+            cols = [dupli(x) for x in first_line]
         return cols
 
-    def reader():
-        global rows
-        global firstLine
-        global lineReader
-        finalJson = []
-        if isinstance(pathToFile, str):
-            firstLine = next(lineReader)
-        elif isinstance(pathToFile, list):
-            firstLine = lineReader[0].split(options["separator"])
-            lineReader = lineReader[1:]
-        rows = parsefirstLine()
-        for oneLine in lineReader:
-            parsedLine = {}
-            if isinstance(pathToFile, list):
-                oneLine = oneLine.split(options["separator"])
-            elif isinstance(pathToFile, str) and isinstance(oneLine, list) and len(oneLine) == 0:
-                oneLine = [''] #create a fake void line
-            if options["avoidVoidLine"] == True:
-                if (isinstance(oneLine, list) and len(oneLine) == 0) or (isinstance(oneLine, list) and len(oneLine) >= 1 and oneLine[0] == "") or oneLine == "" or oneLine == "\n" or oneLine == "\r\n":
+    def reader(line_reader):
+        """" Read the file """
+        final_json = []
+        if isinstance(path_to_file, str):
+            first_line = next(line_reader)
+        elif isinstance(path_to_file, list):
+            first_line = line_reader[0].split(options["separator"])
+            line_reader = line_reader[1:]
+        if isinstance(options["overrideFirstLine"], list):
+            first_line = options["overrideFirstLine"]
+        rows = parse_first_line(first_line)
+        for one_line in line_reader:
+            parsed_line = {}
+            if isinstance(path_to_file, list):
+                one_line = one_line.split(options["separator"])
+            elif isinstance(path_to_file, str) and isinstance(one_line, list) and not one_line:
+                one_line = [''] #create a fake void line
+            if options["avoidVoidLine"]:
+                if (isinstance(one_line, list) and not one_line) or (isinstance(one_line, list) and len(one_line) >= 1 and one_line[0] == "") or one_line == "" or one_line == "\n" or one_line == "\r\n":
                     continue
-            parsedLine = parseLine(oneLine)
+            parsed_line = parse_line(one_line, rows, first_line)
             if callable(options["lineCallBack"]):
-                resCallback = options["lineCallBack"](parsedLine, oneLine)
-                if resCallback == None:
+                res_callback = options["lineCallBack"](parsed_line, one_line)
+                if res_callback is None:
                     if options["callBackForce"]:
-                        parsedLine = resCallback
+                        parsed_line = res_callback
                     else:
                         if options["debug"]:
                             print(
                                 "CallBack force at False and callBack result is not correct")
                 else:
-                    parsedLine = resCallback
-            finalJson.append(parsedLine)
-        return finalJson
-    converted = reader()
-    if isinstance(pathToFile, str):
-        csvFile.close()
+                    parsed_line = res_callback
+            final_json.append(parsed_line)
+        return final_json
+    converted = reader(line_reader)
+    if isinstance(path_to_file, str):
+        csv_file.close()
     return converted
